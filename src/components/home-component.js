b39f43768b77277fc36183dbd9ac985042cb194e
Pascal Pascarella <pascalpascarella86@gmail.com>
2020-04-07 21:16:39 -0500

    Correct indent to (Tab-Size:2) in home-component.js

--- src/components/home-component.js
+++ src/components/home-component.js
@@ -8,29 +8,28 @@
 ===========================================
 */
 ko.components.register("homecomponent", {
-    viewModel: function(){
-        var self = this;
-        self.vm = ViewModel;    // Reference the main App VM.
-        /*
-         * Determines whether the big button should say
-         * resume or start. Bases decision off of the
-         * initialized observable from the main app viewModel.
-        */
-        self.quizMessage = ko.computed(function(){
-            if(self.vm.initialized() === false){
-                return "START QUIZ";
-            }else{
-                return "RESUME QUIZ";
-            } 
-        }, self);
-    },
-    template:
-    '\
-        <div class="home-main">\
-            <div class="home-background"></div>\
-            <div class="home-text-box">\
-                <div class="home-title">TRY/CATCH!</div>\
-                <div class="home-tagline">AN SPA QUIZZER FOR JAVASCRIPT ENTHUSIASTS</div>\
-                <div data-bind="text: quizMessage" role="button" class="home-start-button unselectable">START QUIZ</div>\
-        </div>'
+	viewModel: function(){
+		var self = this;
+		self.vm = ViewModel;    // Reference the main App VM.
+		/*
+		 * Determines whether the big button should say
+		 * resume or start. Bases decision off of the
+		 * initialized observable from the main app viewModel.
+		*/
+		self.quizMessage = ko.computed(function(){
+			if(self.vm.initialized() === false){
+				return "START QUIZ";
+			}else{
+				return "RESUME QUIZ";
+			}
+		}, self);
+	},
+	template:
+ '<div class="home-main">\
+		<div class="home-background"></div>\
+			<div class="home-text-box">\
+				<div class="home-title">TRY/CATCH!</div>\
+				<div class="home-tagline">AN SPA QUIZZER FOR JAVASCRIPT ENTHUSIASTS</div>\
+				<div data-bind="text: quizMessage" role="button" class="home-start-button unselectable">START QUIZ</div>\
+	</div>'
 });
